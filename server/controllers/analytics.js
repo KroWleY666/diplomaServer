4260ed416abe0576e8cd8347e165bee0e3f1903b
Kristina <kristina8625@mail.ru>
2020-06-04 17:51:21 +0700

    аналитика

--- server/controllers/analytics.js
+++ server/controllers/analytics.js
@@ -12,7 +12,14 @@ module.exports = {
             participant_id: req.params.participant_id,
             stn_id: req.params.stn_id
         }, attributes: [ 'data', 'value'], order: [['data', 'ASC']]}) // return moment.utc(this.getDataValue('data')).format('%d-%m-%Y');//DD-MM-YYYY
-        .then(standart => {return res.status(200).send({standart, message: 'Поиск прошел учпешно!'})})
+        .then(standart => {
+          var mas1 = []
+          var mas2 = []
+          for(let h in standart){
+            mas1[h] = standart[h].data
+            mas2[h] = standart[h].value
+          }
+          return res.status(200).send({mas1,mas2})})
         .catch((error) => res.status(400).send({error, message: 'Поиск провалился!'}))
     })
     .catch(error => res.status(400).send(error));        
@@ -26,7 +33,14 @@ module.exports = {
               participant_id: req.params.participant_id,
               pn_id: req.params.pn_id
           }, attributes: ['data', 'value'], order: [['data', 'ASC']]})
-          .then(standart => {return res.status(200).send({standart, message: 'Поиск прошел учпешно!'})})
+          .then(standart => {
+            var mas1 = []
+            var mas2 = []
+            for(let h in standart){
+              mas1[h] = standart[h].data
+              mas2[h] = standart[h].value
+            }
+            return res.status(200).send({mas1,mas2})})
           .catch(error => res.status(400).send({error, message: 'Параметр не найден!'}))
         })
       .catch(error => res.status(400).send(error));        
